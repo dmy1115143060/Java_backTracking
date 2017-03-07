1591ece573f470bc153342d175d68974d0e30d0e
dmy1115143060 <1115143060@qq.com>
2017-03-07 21:47:09 +0800

    Update Combination_Sum_IV_42.java

--- Combination_Sum_IV_42.java
+++ Combination_Sum_IV_42.java
@@ -26,10 +26,10 @@ import java.util.List;
  * 解题思路：利用回溯法解决，利用List来存储每个序列，同样最后的得到的序列也存储到List中.
  * 得到的序列只要不同均为合法解
  */
-public class Combination_Sum_IV_42
-{
-	public static void main(String[] args)
-	{
+public class Combination_Sum_IV_42{
+	
+	public static void main(String[] args){
+		
 		int[] nums={1, 2, 3};
 		int target = 4;
 		int start = 0;
@@ -48,20 +48,22 @@ public class Combination_Sum_IV_42
 	 * @param targetSeq:最终得到的满足target的目标序列的集合
 	 * @param currentSeq:当前数字序列
 	 */
-	public static void getCombinationSumSeq(int[] nums, int target, int start, List<List<Integer>> targetSeq, List<Integer> currentSeq)
-	{
-		for(int i = start; i < nums.length; i++)
-		{
-			if(nums[i] == target)
-			{
+	public static void getCombinationSumSeq(int[] nums, int target, int start, List<List<Integer>> targetSeq, 
+						List<Integer> currentSeq){
+		
+		for(int i = start; i < nums.length; i++){
+			
+			if(nums[i] == target){
+				
 				List<Integer> newCurrentSeq = new ArrayList<>(currentSeq);
 				newCurrentSeq.add(nums[i]);
 				targetSeq.add(newCurrentSeq);
 			}
+			
 			//当前数字比target小，此时将数字加入到currentSeq中，并且将target值更新为target-nums[i],
 			//继续遍历start号元素(由于不同序列的元素也符合要求，因此每次都会从初始位置进行遍历)
-			else if(nums[i] < target)
-			{
+			else if(nums[i] < target){
+				
 				List<Integer> newCurrentSeq = new ArrayList<>(currentSeq);
 				newCurrentSeq.add(nums[i]);
 				getCombinationSumSeq(nums, target - nums[i], start, targetSeq, newCurrentSeq);
@@ -69,6 +71,5 @@ public class Combination_Sum_IV_42
 			else
 				return;
 		}
-
 	}
 }
