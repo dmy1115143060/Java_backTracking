aed01a7f6f01a01d3a3c88c56a2975bb2d842794
杜名洋 <1115143060@qq.com>
2017-03-08 10:29:06 +0800

    Update Permutation_Sequence_60.java

--- Permutation_Sequence_60.java
+++ Permutation_Sequence_60.java
@@ -2,8 +2,6 @@ package LeetCode.BackTrack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 /**
  * The set [1,2,3,…,n] contains a total of n! unique permutations.
    By listing and labeling all of the permutations in order,
@@ -18,49 +16,71 @@ import java.util.Scanner;
  */
 public class Permutation_Sequence_60
 {
-
-    private static List<String> resultList = new ArrayList<>();
+    private int count = 0;
+    private int[] nums;
+    private StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        int n, k;
-        String source = "";
-        n = input.nextInt();
-        k = input.nextInt();
-        for(int i = 1; i <= n; i++)
-            source += i;
-        backTrack(source,0, new ArrayList<>());
-
-        for(String s : resultList)
-            if(s.startsWith(k + ""))
-                System.out.println(s);
+        System.out.println(new Permutation_Sequence_60().getPermutation(10,20));
     }
 
-    public static void backTrack(String source, int start, List<Character> list)
+    public String getPermutation(int n, int k)
     {
-        for(int i = 0; i < source.length(); i++)
+        nums = new int[n];
+
+        int fac = 1;
+        for(int i=2; i<n; i++)
+            fac *= i;
+        for(int i=0; i<n; i++)
+            nums[i] = i+1;
+
+        //计算k是属于第几组，对于n!来说，可以看成分成 n 组，每一组有 (n-1)！个数，第一组起始数字 1，第二组起始数字 2，第 i 组起始数字 i
+        //classNO 对应的组起始数字应该为 classNO,在数组中下标应该为 classNO-1
+        int classNO = 0;
+        for(int level = 1; level<=n; level++)
         {
-            if( !list.contains(source.charAt(i)))
+            if(k <= level * fac)
             {
-                List<Character> newList = new ArrayList<>(list);
-                newList.add(source.charAt(i));
-                if(newList.size() == source.length())
-                    resultList.add(getString(newList));
-                backTrack(source, start, newList);
+                classNO = level;
+                break;
             }
-            else
-                continue;
         }
+
+        //此时将classNO对应的元素放在起始位置,即进行移位
+        for(int i=classNO-2; i>=0; i--)
+            nums[i+1] = nums[i];
+        nums[0] = classNO;
+
+        count = 0;
+        backTrack(0, new ArrayList<>(), n, k - (classNO-1)*fac, new boolean[n]);
+        return sb.toString();
     }
 
-    public static String getString(List<Character> list)
+    public void backTrack(int start, List<Integer> list, int n, int k, boolean[] used)
     {
-        String s = "";
-        for (char c : list)
+        if(count == k) //当遍历到符合条件的第 k 个元素时，退出
+            return;
+
+        for(int i=start; i<n; i++)
         {
-            s += Character.toString(c);
+            if(used[i]) //当前元素被使用过，则尝试下一个元素
+                continue;
+
+            used[i] = true; //标记元素被使用过
+            list.add(nums[i]);
+            if(list.size() == n)
+            {
+                count++;
+                if(count == k)
+                    for(Integer num : list)
+                        sb.append(num);
+                //System.out.println(list);
+            }
+
+            backTrack(start, list, n, k, used);
+            list.remove(list.size() - 1);
+            used[i] = false;
         }
-        return s;
     }
 }
