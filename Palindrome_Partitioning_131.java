8855db209c1b78b60a75c52c38d7b34e282f7958
杜名洋 <1115143060@qq.com>
2017-03-12 21:45:43 +0800

    Update Palindrome_Partitioning_131.java

--- Palindrome_Partitioning_131.java
+++ Palindrome_Partitioning_131.java
@@ -1,93 +1,72 @@
 package LeetCode.BackTrack;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * 利用回溯法求出一个字符串的的回文子串，并且保证这些回文子串可以构成原串
+ *  Given a string s, partition s such that every substring of the partition is a palindrome.
+    Return all possible palindrome partitioning of s.
+    For example, given s = "aab",
+    Return
+    [
+        ["aa","b"],
+        ["a","a","b"]
+    ]
  */
 public class Palindrome_Partitioning_131
 {
-    //利用Set来保存不同的回文子串组合序列
-    private static List<List<String>> result = new ArrayList<>();
-    private static String source = "abba";
+    private List<String> currList;
+    private List<List<String>> result;
 
     public static void main(String[] args)
     {
-        backTrack(0, source, getPalindromeStr(source), new ArrayList<>());
-        System.out.println("=======================================");
-        for(List<String> l : result)
-            System.out.println(l);
+        Palindrome_Partitioning_131 solution = new Palindrome_Partitioning_131();
+        System.out.println(solution.partition("aabaa"));
     }
 
-    /**
-     * @param start
-     * @param palindromeList:保存所有的字符串的所有的子回文串
-     * @param list：保存当前符合要求的子回文串
-     */
-    public static void backTrack(int start, String str, List<String> palindromeList, List<String> list)
+    public List<List<String>> partition(String s)
     {
-        for(int i = start; i < palindromeList.size(); i++)
+        currList = new ArrayList<>();
+        result = new ArrayList<>();
+        backTrack(s,0);
+        return result;
+    }
+
+    public void backTrack(String str, int l)
+    {
+        if(currList.size() > 0 && l>=str.length())
+            result.add(new ArrayList<>(currList));
+
+        for(int i=l; i<str.length(); i++)
         {
-            if(list.size() < str.length() && str.startsWith(getStr(list)))
+            if(isPalindrome(str,l,i))//判断当前 l->i 这个子串是否是回文串
             {
-                list.add(palindromeList.get(i));
-                if(getStr(list).equals(str))
-                    if(!result.contains(list))
-                        result.add(new ArrayList<>(list));
-                backTrack(start, str, palindromeList, list);
-                list.remove(list.size() - 1);
+                if(l == i) // l==i 说明当前子串长度为1，直接加入
+                    currList.add(Character.toString(str.charAt(i)));
+                else //不等，说明需要将子串加入（由判断条件决定此子串肯定是回文子串）
+                    currList.add(str.substring(l,i+1));
+                backTrack(str,i+1);//递归判断后续子串
+                currList.remove(currList.size()-1);
             }
-            else
-                return;
         }
-    }
-
-    /**
-     * 按照从前往后获取一个字符串的所有可能回文子串
-     */
-    public static List<String> getPalindromeStr(String str)
-    {
-        List<String> palindromeList = new ArrayList<>();
-
-        for(int i = 0; i < str.length(); i++)
-        {
-            String s = Character.toString(str.charAt(i));
-            if(!palindromeList.contains(s))
-                palindromeList.add(s);
-        }
-
-        for(int i = 1; i < str.length(); i++)
-            for(int j = i + 1; j <= str.length(); j++)
-            {
-                String s = str.substring(i, j);
-                if(isPalindrome(s) && !palindromeList.contains(s))
-                    palindromeList.add(s);
-
-            }
-        return palindromeList;
-    }
-
-    public static String getStr(List<String> list)
-    {
-        String str = "";
-        for(String s : list)
-            str += s;
-        return str;
     }
 
     /**
      * 判断一个字符串是否是回文串
      */
-    public static boolean isPalindrome(String str)
+    public static boolean isPalindrome(String str, int l, int r)
     {
-        for(int i = 0, j = str.length()-1; i <= j; i++, j--)
+        if(l == r)
+            return true;
+        while(l < r)
         {
-            if(str.charAt(i) != str.charAt(j))
-               return false;
+            if(str.charAt(l) != str.charAt(r))
+                return false;
+            l++;
+            r--;
         }
         return true;
     }
 }
+
